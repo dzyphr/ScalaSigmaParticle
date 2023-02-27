aa105269905618df67e9816c5c9b21d593c8eacd
justaguy12345 <ryanberk135@gmail.com>
2023-02-27 17:10:26 -0600

    Scala Sigma Paricle first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Sigma Particle
+
+[![SigmaParticlePicture](https://d2r55xnwy6nx47.cloudfront.net/uploads/2016/11/1_no-glowing.jpg)](https://github.com/dzyphr/ScalaSigmaParticle)
+
+#### Right now just a collection of working scala ErgoScript examples
+
+#### In the future will aim to reduce boilerplate setup through modular functions
+
+#### Also planning to connect to python version for cross-lang compatibility
+
+View EXAMPLECONFIG for setting up personal variables, when running examples make sure to give the name of the config you make as the arg
+
+Tested with Intellij Idea
---
+++ basic_framework/src/main/scala/UnfreezeCoin.scala
@@ -0,0 +1,63 @@
+import org.ergoplatform.appkit._
+import org.ergoplatform.appkit.config.{ErgoNodeConfig, ErgoToolConfig}
+
+object UnfreezeCoin {
+  def unfreeze(configFileName: String): String = {
+    val config: ErgoToolConfig = ErgoToolConfig.load(configFileName)
+    val nodeConfig: ErgoNodeConfig = config.getNode
+    val explorerUrl: String = "https://tn-ergo-explorer.anetabtc.io/"
+
+    val sender: Address = Address.create(config.getParameters.get("senderAddr"))
+    val addrIndex: Int = config.getParameters.get("addrIndex").toInt
+    val frozenCoins = Address.create(config.getParameters.get("freezeAddress"))
+    println(sender.asP2PK())
+
+    val ergoClient: ErgoClient = RestApiErgoClient.create(nodeConfig, explorerUrl)
+
+    val txJson: String = ergoClient.execute((ctx: BlockchainContext) => {
+      val walletMnemonic: String = nodeConfig.getWallet().getMnemonic().toString
+      val mnemonicPassword: String = nodeConfig.getWallet().getMnemonicPassword().toString
+      val ssM: SecretString = SecretString.create(walletMnemonic)
+      val ssP: SecretString = SecretString.create(mnemonicPassword)
+
+
+      val prover: ErgoProver = ctx.newProverBuilder().withMnemonic(ssM, ssP, false)
+        .withEip3Secret(addrIndex)
+        .build()
+      println(prover.getEip3Addresses.get(0))
+
+      val senderEIP3: Address = prover.getEip3Addresses().get(0)
+
+      val ergoAmountFeeSubtracted: Long = Parameters.OneErg * 2 - Parameters.MinFee
+      //HEADS UP: if you dont take out the full amount it will create a secondary contract which you can withdraw from
+      //The secondary address may look like a regular address instead of a script
+      val inputboxes = BoxOperations.createForSender(frozenCoins, ctx)
+        .withAmountToSpend(ergoAmountFeeSubtracted + Parameters.MinFee )
+        .loadTop()
+      val signed = BoxOperations.spendBoxesTx(
+        ctx,
+        ctx.newTxBuilder(),
+        inputboxes,
+        prover, sender,
+        ergoAmountFeeSubtracted,
+        Parameters.MinFee
+      )
+      ctx.sendTransaction(signed)
+      signed.toJson(true)
+
+    })
+    txJson
+  }
+  def main(args: Array[String]): Unit = {
+    if (args.length > 1) {
+      println("too many args, enter one arg for config file name")
+      sys.exit
+    }
+    else if (args.length == 0) {
+      print("enter the config file name as the only argument")
+      sys.exit
+    }
+    val txJson: String = unfreeze(args(0))
+    println(txJson)
+  }
+}
